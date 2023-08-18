d5e5b65380eaee11698e424b15851c506edb7eb1
Jes Jes <jvillalont94@gmail.com>
2023-08-17 20:19:31 -0600

    Ultimo intento a la chingada

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -3,10 +3,10 @@ import axios from 'axios';
 const clienteAxios = axios.create({
     baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
     headers: {
-        "Access-Control-Allow-Origin": `${import.meta.env.FRONTEND_URL}/api`
-        /* 'origin':'x-requested-with',
+        "Access-Control-Allow-Origin": `${import.meta.env.FRONTEND_URL}/api`,
+        'origin':'x-requested-with',
         'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
-        'Content-Type': 'application/json' */
+        'Content-Type': 'application/json'
     } 
 });
 
